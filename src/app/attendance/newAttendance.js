4543384c285bfcadfee25714e783141548a0ae84
gkesztyus <gkesztyus@gmail.com>
2017-07-13 15:37:03 +0200

    fixing datepicker to be able to store in state

--- src/app/attendance/newAttendance.js
+++ src/app/attendance/newAttendance.js
@@ -1,8 +1,14 @@
-/* eslint-disable no-alert */
+/* eslint-disable react/prop-types */
 import React, {Component} from 'react';
-import {TrainingDatePicker} from './trainingDatePicker';
 import PropTypes from 'prop-types';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
 
+import 'react-datepicker/dist/react-datepicker.css';
+/* https://github.com/Hacker0x01/react-datepicker
+https://hacker0x01.github.io/react-datepicker/#example-5 */
+
+// a dátum mentését majd .toString-gel kellene megcsinálni és a szerkesztésnél figyelni rá hogy mifaszvan...
 const styles = {
   attendance: {
     fontWeight: 'bold',
@@ -15,8 +21,15 @@ export class NewAttendance extends Component {
     super(props);
     this.handleCancel = this.handleCancel.bind(this);
     this.handleSave = this.handleSave.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
     this.state = {
-
+      newTrainingData: {
+        date: moment(),
+        lead: [],
+        attendees: [],
+        theme: ''
+      }
     };
   }
   handleCancel() {
@@ -25,6 +38,29 @@ export class NewAttendance extends Component {
   }
   handleSave() {
     console.log('save!!!', this);
+  }
+  handleInputChange(event) {
+    console.log('this.state', this.state);
+    console.log('event target', event.target);
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    console.log(value);
+
+    this.setState({
+      newTrainingData: {
+        [name]: value
+      }
+    });
+    console.log(this.state);
+  }
+  handleDateChange(dateValue) {
+    this.setState({
+      newTrainingData: {
+        date: dateValue
+      }
+    });
+    console.log(this);
   }
   render() {
     return (
@@ -39,17 +75,35 @@ export class NewAttendance extends Component {
                 <div className="col-lg-2 col-xs-12">
                   <div className="form-group">
                     <label>Edzés időpontja</label>
-                    <TrainingDatePicker/>
+                    <div className="hidden-xs">
+                      <DatePicker
+                        onChange={this.handleDateChange}
+                        selected={this.state.newTrainingData.date}
+                        locale="hu-hu"
+                        todayButton={'Mai napon'}
+                        dateFormat="YYYY/MM/DD"
+                        />
+                    </div>
+                    <div className="hidden-sm hidden-md hidden-lg">
+                      <DatePicker
+                        onChange={this.handleDateChange}
+                        selected={this.state.newTrainingData.date}
+                        todayButton={'Mai napon'}
+                        dateFormat="YYYY/MM/DD"
+                        locale="hu-hu"
+                        withPortal
+                        />
+                    </div>
                   </div>
                 </div>
                 <div className="col-lg-3 col-sm-6 col-xs-12">
                   <div className="form-group">
                     <label>Edzésvezető</label>
-                    <select multiple className="form-control">
-                      <option>Kesztyűs Gábor</option>
-                      <option>Kéri Dávid</option>
-                      <option>Hegyes Endre</option>
-                      <option>Bugris Valéria</option>
+                    <select name="lead" value={this.state.newTrainingData.lead} onChange={this.handleInputChange} multiple className="form-control">
+                      <option value="Kesztyűs Gábor">Kesztyűs Gábor</option>
+                      <option value="Kéri Dávid">Kéri Dávid</option>
+                      <option value="Hegyes Endre">Hegyes Endre</option>
+                      <option value="Bugris Valéria">Bugris Valéria</option>
                     </select>
                   </div>
                 </div>
@@ -67,12 +121,12 @@ export class NewAttendance extends Component {
                 <div className="col-lg-4 col-xs-12">
                   <div className="form-group">
                     <label>Edzés témája</label>
-                    <textarea className="form-control" rows="3"/>
+                    <textarea name="theme" value={this.state.newTrainingData.theme} onChange={this.handleInputChange} className="form-control" rows="3"/>
                   </div>
                 </div>
                 <div className="col-xs-12">
                   <div className="btn-group" role="group">
-                    <button type="submit" className="btn btn-primary" onClick={this.handleSave}>Mentés</button>
+                    <button type="button" className="btn btn-primary" onClick={this.handleSave}>Mentés</button>
                     <button type="button" className="btn btn-default" onClick={this.handleCancel}>Mégsem</button>
                   </div>
                 </div>
