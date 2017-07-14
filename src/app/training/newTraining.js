053a196cc874cf4cc17d7b231484f3129c9d5211
gkesztyus <gkesztyus@gmail.com>
2017-07-14 14:22:43 +0200

    fixing data handling and adding new multiselect

--- src/app/training/newTraining.js
+++ src/app/training/newTraining.js
@@ -1,12 +1,12 @@
-/* eslint-disable react/prop-types */
+/* eslint-enable */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import DatePicker from 'react-datepicker';
+import SuperSelect from 'react-super-select';
 import moment from 'moment';
-import Multiselect from 'react-bootstrap-multiselect';
 
 import 'react-datepicker/dist/react-datepicker.css';
-import 'react-bootstrap-multiselect/css/bootstrap-multiselect.css';
+import 'react-super-select/lib/react-super-select.css';
 /* https://github.com/Hacker0x01/react-datepicker
 https://hacker0x01.github.io/react-datepicker/#example-5 */
 
@@ -23,27 +23,27 @@ export class NewTraining extends Component {
     super(props);
     this.handleCancel = this.handleCancel.bind(this);
     this.handleSave = this.handleSave.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleThemeChange = this.handleThemeChange.bind(this);
     this.handleDateChange = this.handleDateChange.bind(this);
-    this.handleLeadChange = this.handleLeadChange.bind(this);
+    this.handleLeadsChange = this.handleLeadsChange.bind(this);
+    this.handleParticipantsChange = this.handleParticipantsChange.bind(this);
     this.state = {
       newTrainingData: {
         date: moment(),
-        lead: [],
-        attendees: [],
+        leads: [],
+        participants: [],
         theme: ''
       },
       data: {
         people: [
-          {value: 'Kesztyűs Gábor István', label: 'Kesztyűs Gábor'},
-          {value: 'Kéri Dávid'},
-          {value: 'Hegyes Endre'},
-          {value: 'Bugris Valéria'}
+          {id: 1, value: 'Kesztyűs Gábor István', name: 'Kesztyűs Gábor'},
+          {id: 2, value: 'Kéri Dávid', name: 'Kéri Dávid'},
+          {id: 3, value: 'Hegyes Endre', name: 'Hegyes Endre'},
+          {id: 4, value: 'Bugris Valéria', name: 'Bugris Valéria'},
+          {id: 5, value: 'Fitzgerald Kirby', name: 'Angelina Pierce'}, {id: 6, value: 'Earline Terrell', name: 'Hopkins Sosa'}, {id: 7, value: 'Miranda Strickland', name: 'Sosa Blanchard'}, {id: 8, value: 'Evangeline Cole', name: 'Morin Patterson'}, {id: 9, value: 'Lawson Guthrie', name: 'Bernadine Macdonald'}, {id: 10, value: 'Bates Flowers', name: 'Scott Morrow'}, {id: 11, value: 'Lana Wooten', name: 'Nancy Blankenship'}, {id: 12, value: 'Rowena Schneider', name: 'Griffin Michael'}, {id: 13, value: 'Tiffany Knight', name: 'Clark Russo'}, {id: 14, value: 'Gross Daugherty', name: 'Hahn Coffey'}, {id: 15, value: 'Howell Chapman', name: 'Tami Silva'}, {id: 16, value: 'Melendez Melton', name: 'Fran Ratliff'}, {id: 17, value: 'Ashley Christensen', name: 'Araceli Sullivan'}, {id: 18, value: 'Pierce Fry', name: 'Aurora Curtis'}, {id: 19, value: 'Hyde Gardner', name: 'Wilkinson Oneil'}
         ]
       }
     };
-
-    window.myState = this.state;
   }
   handleCancel() {
     // console.log('cancel!!!', this.props);
@@ -52,39 +52,31 @@ export class NewTraining extends Component {
   handleSave() {
     console.log('save!!!', this);
   }
-  handleInputChange(event) {
-    console.log('this.state', this.state);
-    console.log('event target', event.target);
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-    console.log(value);
-
-    this.setState({
-      newTrainingData: {
-        [name]: value
-      }
-    });
-    console.log(this.state);
+  handleThemeChange(e) {
+    const newState = this.state;
+    newState.newTrainingData.theme = e.target.value;
+    this.setState(newState);
   }
-  handleDateChange(dateValue) {
-    this.setState({
-      newTrainingData: {
-        date: dateValue
-      }
-    });
-    console.log(this);
+  handleDateChange(e) {
+    const newState = this.state;
+    newState.newTrainingData.date = e;
+    this.setState(newState);
   }
-  handleLeadChange(leads) {
-    console.log(this, leads);
-    for (const lead of leads) {
-      console.log('lead changed', lead, lead.label, lead.value, this);
-    }
+  handleLeadsChange(e) {
+    const newState = this.state;
+    newState.newTrainingData.leads = e;
+    this.setState(newState);
+  }
+  handleParticipantsChange(e) {
+    const newState = this.state;
+    newState.newTrainingData.participants = e;
+    this.setState(newState);
   }
   render() {
     return (
       <div style={styles.training}>
         <div className="panel panel-default">
+          <div>{JSON.stringify(this.state, undefined, 2)}</div>
           <div className="panel-heading">
               Új edzés felvétele
           </div>
@@ -119,38 +111,31 @@ export class NewTraining extends Component {
                   <div className="form-group">
                     <label>Edzésvezető</label>
                     {/* http://davidstutz.github.io/bootstrap-multiselect/#configuration-options-buttonTitle */}
-                    <Multiselect
-                      data={this.state.data.people}
+                    <SuperSelect
+                      dataSource={this.state.data.people}
+                      placeholder="Válassz edzésvezetőt!"
+                      onChange={this.handleLeadsChange}
                       multiple
-                      maxHeight={200}
-                      nonSelectedText={function () {
-                        return 'Válassz edzésvezetőt!';
-                      }}
-                      onChange={this.handleLeadChange}
+                      keepOpenOnSelection
                       />
-                    <select name="lead" value={this.state.newTrainingData.lead} onChange={this.handleInputChange} multiple className="form-control">
-                      <option value="Kesztyűs Gábor">Kesztyűs Gábor</option>
-                      <option value="Kéri Dávid">Kéri Dávid</option>
-                      <option value="Hegyes Endre">Hegyes Endre</option>
-                      <option value="Bugris Valéria">Bugris Valéria</option>
-                    </select>
                   </div>
                 </div>
                 <div className="col-lg-3 col-sm-6 col-xs-12">
                   <div className="form-group">
                     <label>Résztvevők</label>
-                    <select multiple className="form-control">
-                      <option>Kesztyűs Gábor</option>
-                      <option>Kéri Dávid</option>
-                      <option>Hegyes Endre</option>
-                      <option>Bugris Valéria</option>
-                    </select>
+                    <SuperSelect
+                      dataSource={this.state.data.people}
+                      placeholder="Válassz résztvevőket!"
+                      onChange={this.handleParticipantsChange}
+                      multiple
+                      keepOpenOnSelection
+                      />
                   </div>
                 </div>
                 <div className="col-lg-4 col-xs-12">
                   <div className="form-group">
                     <label>Edzés témája</label>
-                    <textarea name="theme" value={this.state.newTrainingData.theme} onChange={this.handleInputChange} className="form-control" rows="3"/>
+                    <textarea name="theme" value={this.state.newTrainingData.theme} onChange={this.handleThemeChange} className="form-control" rows="3"/>
                   </div>
                 </div>
                 <div className="col-xs-12">
