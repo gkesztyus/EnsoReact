f5cf6b39d7df6c8b19e41db8111c1817f9782b99
gkesztyus <gkesztyus@gmail.com>
2017-08-09 10:11:40 +0200

    remove commented code

--- src/app/transactions/incomeForm.js
+++ src/app/transactions/incomeForm.js
@@ -146,62 +146,6 @@ export class IncomeForm extends Component {
                 />
             </div>
           </div>
-
-          {/* <div className="col-lg-4 col-xs-12">
-            <div className="form-group">
-              <label>Egyéb</label>
-              <input name="otherType" className="form-control"/>
-            </div>
-          </div>
-          <div className="col-lg-4 col-xs-12">
-            <div className="form-group">
-              <label>Telefonszám</label>
-              <input type="tel" name="phoneNumber" value={this.state.newPersonData.phoneNumber} onChange={this.handleSimpleInputChange} className="form-control"/>
-            </div>
-          </div>
-          <div className="col-lg-4 col-xs-12">
-            <div className="form-group">
-              <label>Email</label>
-              <input type="email" name="email" value={this.state.newPersonData.email} onChange={this.handleSimpleInputChange} className="form-control"/>
-            </div>
-          </div>
-          <div className="col-lg-4 col-xs-12">
-            <div className="form-group">
-              <label>Fokozat történet</label>
-              <div className="table-responsive">
-                <table className="table table-striped" style={styles.rankHistoryBox}>
-                  <tbody style={styles.tableBody}>
-                    {this.state.data.availableRanks.map((item, index) => (
-                      <tr key={index}>
-                        <td>{item.name}</td>
-                        <td>
-                          <div className="pull-left">
-                            <DatePicker
-                              onChange={this.handleRankHistoryDateChange[item.value]}
-                              selected={this.state.newPersonData.rankDates[item.value]}
-                              todayButton={'Mai napon'}
-                              dateFormat="YYYY/MM/DD"
-                              locale="hu-hu"
-                              withPortal
-                              />
-                          </div>
-                          <button style={styles.deleteButton} onClick={this.handleRankDelete[item.value]} className={'pull-left btn btn-link ' + (this.state.newPersonData.rankDates[item.value] ? 'show' : 'hidden')}>
-                            <span className="glyphicon glyphicon-remove"/>
-                          </button>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-          <div className="col-xs-12">
-            <div className="btn-group" role="group">
-              <button type="button" className="btn btn-primary" onClick={this.handleSave}>Mentés</button>
-              <button type="button" className="btn btn-default" onClick={this.handleCancel}>Mégsem</button>
-            </div>
-          </div> */}
         </div>
       </form>
     );
