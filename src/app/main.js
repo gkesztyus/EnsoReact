f7402cdf23274c281999550e673efc4d6adf2a9f
gkesztyus <gkesztyus@gmail.com>
2017-08-10 10:18:49 +0200

    before replacing the new menu

--- src/app/main.js
+++ src/app/main.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import {Header} from './header';
 import {Title} from './title';
 import {Techs} from './techs/techs';
 import {Footer} from './footer';
 import {Training} from './training/training';
 import {People} from './people/people';
 import {Transactions} from './transactions/transactions';
+import {Menu} from './menu';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 const styles = {
   container: {
@@ -24,10 +25,14 @@ export class Main extends Component {
   render() {
     return (
       <div style={styles.container}>
-        <Header/>
-        <Training/>
-        <People/>
-        <Transactions/>
+        <Menu/>
+        <BrowserRouter>
+          <div className="container">
+            <Route path="/people" component={People}/>
+            <Route path="/training" component={Training}/>
+            <Route path="/transactions" component={Transactions}/>
+          </div>
+        </BrowserRouter>
         <main style={styles.main}>
           <Title/>
           <Techs/>
