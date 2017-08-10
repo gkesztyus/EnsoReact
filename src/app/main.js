a7d667414ad1f6ea16c629b5e0aa65a82026a3ae
gkesztyus <gkesztyus@gmail.com>
2017-08-10 14:37:18 +0200

    Revert "before replacing the new menu"
    This reverts commit f7402cdf23274c281999550e673efc4d6adf2a9f.

--- src/app/main.js
+++ src/app/main.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
+import {Header} from './header';
 import {Title} from './title';
 import {Techs} from './techs/techs';
 import {Footer} from './footer';
 import {Training} from './training/training';
 import {People} from './people/people';
 import {Transactions} from './transactions/transactions';
-import {Menu} from './menu';
-import {BrowserRouter, Route} from 'react-router-dom';
 
 const styles = {
   container: {
@@ -25,14 +24,10 @@ export class Main extends Component {
   render() {
     return (
       <div style={styles.container}>
-        <Menu/>
-        <BrowserRouter>
-          <div className="container">
-            <Route path="/people" component={People}/>
-            <Route path="/training" component={Training}/>
-            <Route path="/transactions" component={Transactions}/>
-          </div>
-        </BrowserRouter>
+        <Header/>
+        <Training/>
+        <People/>
+        <Transactions/>
         <main style={styles.main}>
           <Title/>
           <Techs/>
